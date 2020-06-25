4eed9531b43681ab0d349cd39ac8bb63addbfaf8
williamjiamin <lxodgf@163.com>
2020-06-25 23:06:13 +0800

    init commit

---
+++ cmake-build-debug/CMakeFiles/7_Other_Types.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "7_Other_Types"
+  "7_Other_Types.pdb"
+  "CMakeFiles/7_Other_Types.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/7_Other_Types.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,25 @@
+//乐学偶得版权所有，主讲人：William 公众号：乐学Fintech 网站：lexueoude.com
+// Created by william from lexueoude.com
+//
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    bool boolean_value = 887876819756;
+
+//    cout << sizeof(boolean_value) << endl;
+    cout << boolean_value << endl;
+
+//    char char_value = 70;
+//    char char_value = 'W';
+//
+//    cout << char_value << endl;
+//    cout << int(char_value) << endl;
+//    cout << "The size of char is " << sizeof(char_value) << endl;
+    return 0;
+
+
+}
